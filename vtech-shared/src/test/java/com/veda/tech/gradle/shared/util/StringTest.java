58177bd24063147339f19179c462efc6b139b17a
Sanjay Balte <sanjay.balte@gmail.com>
2025-08-16 12:05:38 -0400

    postgres

--- vtech-shared/src/test/java/com/veda/tech/gradle/shared/util/StringTest.java
+++ vtech-shared/src/test/java/com/veda/tech/gradle/shared/util/StringTest.java
@@ -1,4 +1,7 @@
 package com.veda.tech.gradle.shared.util;
 
 public class StringTest {
+    public static void main(String[] args) {
+        System.out.println(StringExt.someFun("Hello World"));
+    }
 }
--- vtech-shared/src/test/kotlin/com/veda/tech/gradle/shared/util/StringExt.kt
+++ vtech-shared/src/test/kotlin/com/veda/tech/gradle/shared/util/StringExt.kt
@@ -1,4 +1,11 @@
+@file:JvmName("StringExt")
 package com.veda.tech.gradle.shared.util
 
-class StringExt {
-}
+import io.kotest.common.runBlocking
+
+fun String.someFun(): String = println(this).let { "Hello World $this" }
+
+fun <R> functionWithArgLogic(logic: () -> R) = logic()
+fun <T, R> functionWithArgLogic(obj: T, logic: T.() -> R) = logic(obj)
+fun <T, R> functionWithArgLogic1(obj: T, logic: (T) -> R) = logic(obj)
+fun <T, R> functionWithBlock(obj: T, logic: (T) -> R) = runBlocking { logic(obj) }
